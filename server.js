3ee2df1789bfb800ebbb18ed3e5f63be35137e32
shaunhailey <ediblelysol@gmail.com>
2017-08-08 19:27:47 -0400

    post not working

--- server.js
+++ server.js
@@ -25,23 +25,27 @@ app.set('view engine', 'mst')
 app.get('/', function(req, res) {
   //the below "var word" is confirmed working, providing a random word
   var word = words[Math.floor(Math.random() * words.length)]
-  //wordMasked is splitting word into letters
-  //note: experiementing with transparent text in CSS, check console for text
-  var wordMasked = word.split('')
+  //wordSplitter is splitting word into letters
+  var wordSplitter = word.split('')
+  // var wordMasked = wordSplitter.length('')
 
-  // let hearButton = document.querySelectorAll('button')
-  // hearButton.forEach(button => {
-  //   button.addEventListener('click', event => {
-  //     var button = document.getElementByClassName('letters')
-  //     button.addEventListener('click', hideshow, false)
-  //
-  //     function hideshow() {
-  //       document.getElementByClassName('letters').style.display = 'block'
-  //       this.style.display = 'none'
-  //     }
-  //   })
-  // })
-  res.render('index', { wordMasked: wordMasked })
+  res.render('index', { wordSplitter: wordSplitter })
+})
+// the below captures the letter guesses from the html letter buttons
+app.post('/', (req, res) => {
+  console.log(req.body.letters)
+  let guesses = req.body.letters
+  function hideshow() {
+    for (i=0; i < wordSplitter.length; i++)
+    //guesses is each iterated letter in the wordSplitter string
+    var guesses = letterUp[i]
+    if wordSplitter.includes(guesses)
+    document.getElementByClassName('tiles').style.display = 'block'
+  } else {
+    this.style.display = 'none'
+  }
+
+  })
 })
 
 app.listen(3000, () => {
--- test.js
+++ test.js
@@ -1,12 +1,28 @@
-let hearButton = document.querySelectorAll('button')
-hearButton.forEach(button => {
-  button.addEventListener('click', event => {
-    var button = document.getElementByClassName('letters')
-    button.addEventListener('click', hideshow, false)
-
-    function hideshow() {
-      document.getElementByClassName('letters').style.display = 'block'
-      this.style.display = 'none'
-    }
-  })
-})
+// // let hearButton = document.querySelectorAll('button')
+// // hearButton.forEach(button => {
+// //   button.addEventListener('click', event => {
+// //     var button = document.getElementByClassName('letters')
+// //     button.addEventListener('click', hideshow, false)
+//
+// //     function hideshow() {
+// //       document.getElementByClassName('tiles').style.display = 'block'
+// //       this.style.display = 'none'
+// //     }
+// //   })
+// // })
+// //testing below
+// // app.post('/guessLetter/', (req, res) => {
+// app.post('/', (req, res) => {
+//   //the user-clicked letter or guess
+//   let letterUp = req.body.letters
+//   function hideshow() {
+//     for (i=0; i < wordSplitter.length; i++)
+//     //matcher is each iterated letter in the wordSplitter string
+//     let matcher = letterUp[i]
+//     if wordSplitter.includes(matcher)
+//     document.getElementByClassName('tiles').style.display = 'block'
+//   } else {
+//     this.style.display = 'none'
+//   }
+//
+//   })
